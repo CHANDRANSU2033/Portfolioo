2191b9fba89a1919c9a38d82f344275ac825242b
changabaliye <cse.22bcsd19@silicon.ac.in>
2024-03-02 20:05:37 +0530

    About Done

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,12 +1,14 @@
-import styles from "./App.module.css"
-import { Hero } from "./components/Hero/Hero"
-import { Navbar } from "./components/navbar/Navbar"
+import styles from "./App.module.css";
+import { Hero } from "./components/Hero/Hero";
+import { Navbar } from "./components/navbar/Navbar";
+import About  from "./components/About/About.jsx";
 
 const App = () => {
   return (
     <div className={styles.App}>
       <Navbar/>
       <Hero />
+      <About />
     </div>
   )
 }
---
+++ portfolio/src/components/About/About.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { getImageUrl } from '../../utils'
+import styles from './About.module.css' 
+
+
+const About = () => {
+  return (
+    <section className={styles.container} id='about'>
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <img src = {getImageUrl('about/aboutImage.png')} alt = 'me sitting with a loaptop'
+        className={styles.aboutImage}
+        />
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src = {getImageUrl('about/cursorIcon.png')} alt = 'cursor Icon'/>
+            <div className={styles.aboutItemText}>
+              <h3>FrontEnd Developer</h3>
+              <p>I’m a front-end developer with experience in building responsive and optimized sites</p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src = {getImageUrl('about/serverIcon.png')} alt = 'server Icon'/>
+            <div className={styles.aboutItemText}> 
+              <h3>BackEnd Developer</h3>
+              <p>I have experience developing fast and optimised back-end systems and APIs</p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src = {getImageUrl('about/uiIcon.png')} alt = 'ui Icon'/>
+            <div className={styles.aboutItemText}>
+              <h3>UI Designer</h3>
+              <p>I have designed multiple landing pages and have created design systems as well</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  )
+}
+
+export default About
